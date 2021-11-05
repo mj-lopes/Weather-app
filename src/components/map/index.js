b7354095f6e90c3e08611f7a678c1e71386a7bbd
mlrJunior <marcos_lopes_ribeiro_junior@hotmail.com>
2021-11-04 21:54:36 -0300

    Removido debounce do component Map e adicionado na pasta util

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -1,17 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import { debounce } from "../../util/debounce";
 
 export const Map = () => {
   const locationMap = useSelector((state) => state.todayPrevision.data?.coord);
   const [screenSize, setScreenSize] = useState(0);
-
-  function debounce(func, wait) {
-    let timer = null;
-    return function () {
-      clearTimeout(timer);
-      timer = setTimeout(func, wait);
-    };
-  }
 
   function updateContainerWidth() {
     setScreenSize(window.innerWidth - 15);
@@ -31,7 +24,7 @@ export const Map = () => {
       <iframe
         width={screenSize}
         height="600"
-        src={`https://embed.windy.com/embed2.html?lat=${locationMap.lat}&lon=${locationMap.lon}&zoom=15&level=surface&overlay=rain&product=ecmwf&menu=&message=true&marker=true&calendar=now&pressure=&type=map&location=coordinates&detail=&metricWind=km%2Fh&metricTemp=%C2%B0C&radarRange=-1`}
+        src={`https://embed.windy.com/embed2.html?lat=${locationMap.lat}&lon=${locationMap.lon}&zoom=15&level=surface&overlay=wind&product=ecmwf&menu=&message=true&marker=true&calendar=now&pressure=&type=map&location=coordinates&detail=&metricWind=km%2Fh&metricTemp=%C2%B0C&radarRange=-1`}
         title="Map forescast"
       ></iframe>
     </div>
