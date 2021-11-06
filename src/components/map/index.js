e80a5aa87fde4d9526dc8b52ac7665fe65e3ca60
mlrJunior <marcos_lopes_ribeiro_junior@hotmail.com>
2021-11-06 03:39:36 -0300

    Corrigido bug de espaçamento extra no mapa

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -1,13 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { debounce } from "../../util/debounce";
+import { getDeviceType } from "../../util/getDeviceType";
 
 export const Map = () => {
   const locationMap = useSelector((state) => state.todayPrevision.data?.coord);
   const [screenSize, setScreenSize] = useState(0);
 
   function updateContainerWidth() {
-    setScreenSize(window.innerWidth - 15);
+    const device = getDeviceType();
+    if (device === "mobile" || device === "tablet") {
+      setScreenSize(window.innerWidth);
+    } else {
+      setScreenSize(window.innerWidth - 15);
+    }
   }
 
   useEffect(() => {
@@ -23,7 +29,7 @@ export const Map = () => {
     <div style={{ marginTop: "1rem" }}>
       <iframe
         width={screenSize}
-        height="600"
+        height="500"
         src={`https://embed.windy.com/embed2.html?lat=${locationMap.lat}&lon=${locationMap.lon}&zoom=15&level=surface&overlay=wind&product=ecmwf&menu=&message=true&marker=true&calendar=now&pressure=&type=map&location=coordinates&detail=&metricWind=km%2Fh&metricTemp=%C2%B0C&radarRange=-1`}
         title="Map forescast"
       ></iframe>
